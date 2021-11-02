c3816d802b79e29087a1ac3189e45cb1443b484c
devGeysaLadica <geysa.ladica@gmail.com>
2021-11-02 16:12:11 -0300

    basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,16 @@
+import Datatable from "components/datatable";
+import Footer from "components/footer/index";
+import NavBar from "components/navbar";
+
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-primary">hello world!</h1>
+    <div>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">hello world!</h1>
+        <Datatable />
+      </div>
+      <Footer />
     </div>
   );
 }
